27ddbda5fe015504de7fbcd5d8a6b10256fbaece
nigel-daniels <nigel.daniels@me.com>
2024-11-21 14:09:16 -0800

    Revised OpenAPI section.

--- 5-tools-routing/4_open_api.js
+++ 5-tools-routing/4_open_api.js
@@ -1,29 +1,18 @@
 import { ChatOpenAI } from '@langchain/openai';
-import { JsonSpec, DynamicStructuredTool } from 'langchain/tools';
 import { createOpenAPIChain } from 'langchain/chains';
-import { OpenApiToolkit } from 'langchain/agents/toolkits';
-import { convertToOpenAIFunction } from '@langchain/core/utils/function_calling';
 import * as fs from 'fs';
 
 // Note there is no direct equivalent of the `openapi_spec_to_openai_fn` method
 // This section currently fails to function in LangChain.JS
 let model = new ChatOpenAI({
-  temperature: 0
+	model: 'gpt-3.5-turbo',
+	temperature: 0
 });
 
 // Read the spec as a string
 const petYaml = fs.readFileSync('./petstore.yaml', 'utf8');
 
-// This is an attempt to load the spec as a chain however it does not appear to return a valid chain
-const petChain = createOpenAPIChain(petYaml);
-const petTool = new DynamicStructuredTool(petChain);
-const petFunc = convertToOpenAIFunction(petTool);
+const petChain = await createOpenAPIChain(petYaml, {llm: model});
 
-// For some reason this resolves to an empty schema
-console.log(petFunc);
-
-model = model.bind({functions: [petFunc]});
-
-// This invoke fails with 'TypeError: promptValue.toChatMessages is not a function' due to the schema
-const result = await model.invoke({input: 'What are three pets names?'});
+const result = await petChain.invoke({query: 'What are three pets names?'});
 console.log(result);
